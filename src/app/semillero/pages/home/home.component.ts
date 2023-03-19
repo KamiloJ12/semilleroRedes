d231e58eeea58b37473fba0fcb7563269ca57845
Camilo Jaimes <Camilo12052000@hotmail.com>
2023-03-18 20:16:47 -0500

    modificar header

--- src/app/semillero/pages/home/home.component.ts
+++ src/app/semillero/pages/home/home.component.ts
@@ -4,6 +4,20 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   templateUrl: './home.component.html',
   styles: [
+    `
+      .header {
+        background: url("src/assets/images/background-g4d9ebd0a6.jpg") center center;
+		    background-size: cover;
+        width: 100%;
+        height: 600px;
+      }
+      img {
+        width: 300px;
+        position: absolute;
+        top: 180px;
+        left: 40%;
+      }
+    `
   ]
 })
 export class HomeComponent {
