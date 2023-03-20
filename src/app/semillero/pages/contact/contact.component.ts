f053d74dff7c38a8d6e4091bbe49204891350bfe
BrayanMacias465 <brayan_4655@outlook.com>
2023-03-20 14:51:52 -0500

    dos

--- src/app/semillero/pages/contact/contact.component.ts
+++ src/app/semillero/pages/contact/contact.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  breadCrumbItems!: MenuItem[];
+  home!: MenuItem;
 
+  ngOnInit(): void {
+    this.breadCrumbItems = [
+      { label: 'Contacto' }
+    ];
+    this.home = { icon: 'pi pi-home', routerLink: '/semillero' };
+  }
 }
