991ae02cf4679811f8f3b80540b3ad322d7928f5
Camilo Jaimes <Camilo12052000@hotmail.com>
2023-03-19 21:02:00 -0500

    agregar header about

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
-
+import { CarouselModule } from 'primeng/carousel';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
 
 @NgModule({
   declarations: [],
   imports: [],
   exports: [
-    
+    CarouselModule,
+    BreadcrumbModule
   ]
 })
 export class PrimeNgModule { }
--- src/app/semillero/pages/about-us/about-us.component.ts
+++ src/app/semillero/pages/about-us/about-us.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.css']
 })
-export class AboutUsComponent {
+export class AboutUsComponent implements OnInit {
 
+  breadCrumbItems!: MenuItem[];
+  home!: MenuItem;
+
+  ngOnInit(): void {
+    this.breadCrumbItems = [
+      { label: '¿Quiene somos?' }
+    ];
+    this.home = { icon: 'pi pi-home', routerLink: '/semillero' };
+  }
 }
