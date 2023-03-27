595d876cadb0eb36d2905ce84a6b2d0be9648255
Camilo Jaimes <Camilo12052000@hotmail.com>
2023-03-26 19:19:18 -0500

    modificar pagina actualizar miembros

--- src/app/members/pages/agregar/agregar.component.ts
+++ src/app/members/pages/agregar/agregar.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
   styleUrls: ['./agregar.component.css']
 })
-export class AgregarComponent {
+export class AgregarComponent implements OnInit {
+
+  miembro: any = {};
+  rol: any = {};
+
+  constructor( private activatedRoute: ActivatedRoute,
+               private router: Router ) { }
+
+  ngOnInit(): void {
+    if( this.router.url.includes('editar') ) {
+      if( this.router.url.includes('roles') ) {
+        this.activatedRoute.params.subscribe( id => this.rol.id = id );
+      } else {
+        this.activatedRoute.params.subscribe( id => this.miembro.id = id );
+      }
+      /* this.activatedRoute.params
+        .pipe(
+          switchMap( ({id}) => this.heroesService.getHeroePorId( id ) )
+        )
+        .subscribe( heroe => this.heroe = heroe ); */
+    }
+  }
+
+  cancelar(): void {
+    this.router.navigate(['/miembros/listado']);
+  }
 
 }
